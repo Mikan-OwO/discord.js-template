7ecc6a95ca2269002670dfe321410967a7819e5f
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-09-08 23:54:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const { 
+  Client,
+  MessageEmbed,
+  MessageAttachment
+} = require("discord.js");
 
+const client = new Client();
+
+function Set(message, prefix, [cmd, ...args]) {
+  if(!message.content.startsWith(prefix)) return;
+  if(cmd === "ping") message.channel.send(client.ws.ping + "ms");
+}
+
+client.on("message", message => Set(message, "!", message.content.slice(1).split(" ")));
+
+client.login("token");
